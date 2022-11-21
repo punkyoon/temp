828db7ac3cb29a0780b2120e9360692ccf2334a6
punkyoon <cryptography@konkuk.ac.kr>
2022-11-21 22:56:42 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = '/temp';
+
 const nextConfig = {
+  basePath,
   experimental: { appDir: true, },
   reactStrictMode: true,
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'punkyoon.github.io', port: '', pathname: '/temp/**', },
     ],
+    path: `${basePath}/_next/image`,
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
